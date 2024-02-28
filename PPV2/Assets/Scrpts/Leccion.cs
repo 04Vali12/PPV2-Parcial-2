ba90f90bb2e1be0e4fd8446925d1aca33017f348
Jeshua <Deku050204@gmail.com>
2024-02-27 18:48:52 -0600

    Dualipa 27 Feb 2024

--- PPV2/Assets/Scrpts/Leccion.cs
+++ PPV2/Assets/Scrpts/Leccion.cs
@@ -5,18 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Leccion
 {
-  public int ID;
-  public string lessons;
-  public List<string> options;
-    public int CorrectAnswer;  
- }
-[CreateAssetMenu(fileName = "New subject", menuName = "ScriptableObjects/newLesson", order =1)]
-
-public class Subject : ScriptableObject
-{
-    [Header("GameObject Configuracion")]
-    public int Lesson = 0;
-    [Header("Leccion Quest Configuracion")]
-    public List<Leccion> leccionList;
-
+    public int ID;
+    public string lessons;
+    public List<string> options;
+    public int CorrectAnswer;
 }
--- PPV2/Assets/Scrpts/LevelManager.cs
+++ PPV2/Assets/Scrpts/LevelManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class LevelManager : MonoBehaviour
 {
+    [Header("LevelData")]
+    public Subject Lesson;
+
+    [Header("user interface")]
+    public TMP_Text textQuestion;
+
+    [Header("Game Configuration")]
+    public int questionAmount = 0;
+    public int currentQuestion = 0;
+    public string question;
+    public string CorrectAnswer;
+
+    [Header("Current Lesson")]
+    public Leccion currentLesson;
     // Start is called before the first frame update
     void Start()
     {
-        
+        questionAmount = Lesson.leccionList.Count;
+        LoadQuestion();
     }
 
     // Update is called once per frame
-    void Update()
+    public void LoadQuestion()
     {
-        
+        if (currentQuestion < questionAmount)
+        {
+            currentLesson = Lesson.leccionList[currentQuestion];
+            question = currentLesson.options[currentLesson.CorrectAnswer];
+            CorrectAnswer = currentLesson.options[currentLesson.CorrectAnswer];
+        }
+        else
+        {
+            Debug.Log("Fin de las preguntas");
+        }
     }
+    public void NextQuestion()
+    {
+        if(currentQuestion < questionAmount)
+        {
+            currentQuestion++;
+            LoadQuestion();
+        }
+    }
+
+
 }
