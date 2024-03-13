9418a5382071767d7aa02eb46f2d56f8e3c794b1
Yax <73907804+04Vali12@users.noreply.github.com>
2024-03-12 18:58:55 -0600

    Documwntacion de script option

--- PPV2/Assets/Scrpts/Option.cs
+++ PPV2/Assets/Scrpts/Option.cs
@@ -9,20 +9,27 @@ public class Option : MonoBehaviour
     public int OptionID;
     public string OptionName;
 
-    // Start is called before the first frame update
+    // El componente TMP ayuda a actualizar el texto que tiene
+    // la pregunta en el ScriptableObject
     void Start()
     {
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
     }
 
-
+    //Esta funcion Actualiza el texto
     public void UpdateText()
     {
+        //El componente  Children que contiene el Texto que se actualizará
+        //al que ya está asignado en el ScriptableObject
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
     }
+    //Esta Funcion comprueba que se seleccione una opcion y se llama a dos funciones
+    // del LevelManager
     public void selectOption()
     {
+        //Se asigna la respuesta correcta al ID que se encuentra en el Level manager 
         LevelManager.Instance.SetPlayerAnswer(OptionID);
+        // comprueba si el levelmanager selecciono una respuesta 
         LevelManager.Instance.CheckPlayerState();
     }
 }
